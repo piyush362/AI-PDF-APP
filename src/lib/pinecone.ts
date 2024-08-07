451a58f5f9e5190d14d73d4e7e7a882e60e2e720
amansharma703 <aman703jk@gmail.com>
2024-08-07 10:42:15 +0530

    Fixed : pinecore DB not working

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,4 +1,5 @@
-import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
+// import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
+import { Pinecone, PineconeRecord } from '@pinecone-database/pinecone';
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import md5 from "md5";
@@ -11,7 +12,6 @@ import { convertToAscii } from "./utils";
 
 export const getPineconeClient = () => {
   return new Pinecone({
-    environment: process.env.PINECONE_ENVIRONMENT!,
     apiKey: process.env.PINECONE_API_KEY!,
   });
 };
